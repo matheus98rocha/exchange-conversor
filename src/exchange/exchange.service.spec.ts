fdf5cbc764ba37814675df806c268d350561dd93
matheus98rocha <matheus98rocha@gmail.com>
2023-10-30 22:46:07 -0300

    feat: ensure convertAmount called getCurrency twice

--- src/exchange/exchange.service.spec.ts
+++ src/exchange/exchange.service.spec.ts
@@ -1,21 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { ExchangeService } from './exchange.service';
+import { CurrenciesService, ExchangeService } from './exchange.service';
 import { BadRequestException } from '@nestjs/common';
 
 describe('ExchangeService', () => {
   let service: ExchangeService;
+  let currenciesService: CurrenciesService;
 
   beforeEach(async () => {
+    const currenciesServiceMock = {
+      getCurrency: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ExchangeService],
+      providers: [
+        ExchangeService,
+        { provide: CurrenciesService, useFactory: () => currenciesServiceMock },
+      ],
     }).compile();
 
     service = module.get<ExchangeService>(ExchangeService);
+    currenciesService = module.get<CurrenciesService>(CurrenciesService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
   describe('ConverAmount', () => {
     it('should be throw if called with invalid params', async () => {
       await expect(
@@ -27,6 +37,16 @@ describe('ExchangeService', () => {
       await expect(
         service.convertAmount({ from: 'USD', to: 'BRL', amount: 1 }),
       ).resolves.not.toThrowError();
+    });
+
+    it('should be called getCurrency twice', async () => {
+      await service.convertAmount({ from: 'USD', to: 'BRL', amount: 1 });
+      await expect(currenciesService.getCurrency).toBeCalledTimes(2);
+    });
+
+    it('should be called getCurrency with correct params', async () => {
+      await service.convertAmount({ from: 'USD', to: 'BRL', amount: 1 });
+      await expect(currenciesService.getCurrency).toBeCalledTimes(2);
     });
   });
 });
--- src/exchange/exchange.service.ts
+++ src/exchange/exchange.service.ts
@@ -1,10 +1,20 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 
+export class CurrenciesService {
+  async getCurrency(currency: string): Promise<any> {
+    return currency;
+  }
+}
+
 @Injectable()
 export class ExchangeService {
+  constructor(private currenciesServices: CurrenciesService) {}
   async convertAmount({ from, to, amount }): Promise<any> {
     if (!from || !to || !amount) {
       throw new BadRequestException();
     }
+
+    const currencyFrom = this.currenciesServices.getCurrency(from);
+    const currencyTo = this.currenciesServices.getCurrency(to);
   }
 }
